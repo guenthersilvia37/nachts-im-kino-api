8f42cae701cde131671747cd7432008bf79cca01
guenthersilvia37 <guenthersilvia30@web.de>
2026-02-03 14:04:27 +0100

    server.js aktualisieren

--- server.js
+++ server.js
@@ -553,7 +553,7 @@ app.get("/api/poster", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server läuft auf Port", PORT);
