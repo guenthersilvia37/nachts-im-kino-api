9957a1c0947dc187871132b4331def7c8ae3c8e1
guenthersilvia37 <guenthersilvia30@web.de>
2026-02-03 13:59:34 +0100

    server.js aktualisieren

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const app = express();
 const PORT = process.env.PORT;
